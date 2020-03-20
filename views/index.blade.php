fdd5f356d2131c487bd258cec15d63a937491ea0
青山広平 <aoyamakouhei@aoyamakouheis-MacBook-Pro.local>
2020-03-20 23:28:17 +0900

    Add category picture.

--- views/index.blade.php
+++ views/index.blade.php
@@ -14,6 +14,19 @@
             @while (have_posts())
                 <?php the_post(); ?>
                 <article>
+                    @php
+                        $categories = get_the_category();
+
+                        foreach ($categories as $category) {
+                        $cat_id = $category->cat_ID; // カテゴリのIDを取得
+                        $post_id = 'category_'. $cat_id; // カテゴリIDを付けて、category_1 の様な形にする 
+                        $cat_imgid = get_field('cat_img', $post_id); // cat_imgはフィールド名を入力 
+
+                        $cat_img = wp_get_attachment_image_src($cat_imgid, 'full')[0];
+                        
+                        echo '<img src="'. $cat_img .'">'; 
+                        }
+                    @endphp
                     <h3>{{ the_title() }}</h3>
                     <div>{{ the_content() }}</div>
                 </article>
