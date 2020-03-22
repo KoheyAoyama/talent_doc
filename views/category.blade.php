589a5d31e7f532e9e449ebf6ff68cabab877d638
KoheyAoyama <kou_713@hotmail.com>
2020-03-22 18:00:23 +0900

    Modify category page shows talent info even if there is no post.

--- views/category.blade.php
+++ views/category.blade.php
@@ -2,30 +2,36 @@
 
 {{-- Start Getting the category informatoin --}}
 @php
-    $categories = get_the_category(); // Get the category information
-
-    foreach ($categories as $category) {
-//        $category_img = wp_get_attachment_image_src(get_field('category_img', 'category_'. $category->cat_ID), 'full')[0];
-        $category_img = get_category_img( $category->cat_ID );
+    $category = get_queried_object(); // Get the current page's infromation.
+        $category_id = $category->cat_ID;
+        $category_img = get_category_img( $category_id );
         $category_name = $category->name;
-        $category_description = $category->description;
-    }
+        $category_description = $category->description;   
 @endphp
 {{-- End Getting the category informatoin --}}
 
 @section('content')
     <h1>{{ $category_name }}</h1>
+
     <img src="{{ $category_img }}" alt="{{ $category_name . "のプロフィール写真" }}">
     <p>{{ $category_description }}</p>
 
-    @if (have_posts())
-        @while (have_posts())
-            <?php the_post(); ?>
-            <article>
-                <h3>{{ the_title() }}</h3>
-                <div>{{ the_excerpt() }}</div>
-                <div>{{ get_field('rating_latest') }}</div>
-            </article>
-        @endwhile
-    @endif
+    <div>
+        <p>評価：{{ get_field('rating_latest') }}</p>
+        <p>所属事務所：{{ get_field('talent_office', 'category_'. $category_id) }}</p>
+    </div>
+
+    <section>
+        <h2>{{ $category_name }}の投稿一覧</h2>
+
+        @if (have_posts())
+            @while (have_posts())
+                {{ the_post() }}
+                <article>
+                    <h3>{{ the_title() }}</h3>
+                    <div>{{ the_excerpt() }}</div> 
+                </article>
+            @endwhile
+        @endif
+    </section>
 @endsection
