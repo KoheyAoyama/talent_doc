b07fd71d853134fc00f76e63aca28ea42b8cab98
KoheyAoyama <kou_713@hotmail.com>
2020-03-22 13:42:52 +0900

    Update inline comment.

--- views/category.blade.php
+++ views/category.blade.php
@@ -3,15 +3,16 @@
     $categories = get_the_category(); // Get the category information
 
     foreach ($categories as $category) {
-        //Get the category image.
+        // Get the category image.
         $cat_id = $category->cat_ID; // Get cateory ID from category info array.
         $post_id = 'category_'. $cat_id; // Format post_id value to "Category_* (*->category id)" by combine cat_id value with prefix "Category_".
         $cat_imgid = get_field('cat_img', $post_id); // Get saved category image post meta id from custom field table.
         $cat_img = wp_get_attachment_image_src($cat_imgid, 'full')[0]; // Get saved image url with post meta id.
 
-        //Get category name.
+        // Get category name.
         $cat_name = $category->name;
 
+        // Get category description.
         $cat_description = $category->description;
     }
 @endphp
