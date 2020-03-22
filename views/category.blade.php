7b1b71db5e3b4fcf76c49ff14241fb6aedff6caf
KoheyAoyama <kou_713@hotmail.com>
2020-03-22 13:34:40 +0900

    Add h1 and category image to category.php.

--- views/category.blade.php
+++ views/category.blade.php
@@ -1,1 +1,17 @@
-unko
+@php
+    $categories = get_the_category(); // Get the category information of curent post as array.
+
+    foreach ($categories as $category) {
+        //Get the category image.
+        $cat_id = $category->cat_ID; // Get cateory ID from category info array.
+        $post_id = 'category_'. $cat_id; // Format post_id value to "Category_* (*->category id)" by combine cat_id value with prefix "Category_".
+        $cat_imgid = get_field('cat_img', $post_id); // Get saved category image post meta id from custom field table.
+        $cat_img = wp_get_attachment_image_src($cat_imgid, 'full')[0]; // Get saved image url with post meta id.
+
+        //Get category name.
+        $cat_name = $category->name;
+    }
+@endphp
+
+<h1>{{ $cat_name }}</h1>
+<img src="{{ $cat_img }}" alt="">
