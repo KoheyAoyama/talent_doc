67076a54bd9b304f7f7adfcc48008328eec31bd1
KoheyAoyama <kou_713@hotmail.com>
2020-03-28 12:09:10 +0900

    Add talent_list apge link to header.

--- views/header.blade.php
+++ views/header.blade.php
@@ -1,5 +1,12 @@
+<?php
+  $page = get_page_by_path( 'talent-list' );
+  $permalink = get_permalink( $page->ID );
+  $page_name = get_the_title( $page->ID );
+?>
+
 <header>
   <nav>
     <a href="/">{{ bloginfo('name') }}</a>
+    <a href="{{ $permalink }}">{{ $page_name }}</a>
   </nav>
 </header>
