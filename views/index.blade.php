323833059505cfc770d89ff96f54c61688b08d2f
KoheyAoyama <kou_713@hotmail.com>
2020-04-10 20:30:57 +0900

    Style feed

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
 
 @section('content')
-    <div class="container">
-        <h2>最新の投稿</h2>
+    <div class="p-feed">
+        <h2 class="p-feed__title">最新の投稿</h2>
             @if (have_posts())
                 @while (have_posts())
                     {{ the_post() }}
