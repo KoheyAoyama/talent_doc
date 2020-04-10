0f884dd4bc1cda733a0d317b92eb7f8c57c0b97f
KoheyAoyama <kou_713@hotmail.com>
2020-04-10 18:19:49 +0900

    Craete article_card component php.

--- views/index.blade.php
+++ views/index.blade.php
@@ -3,42 +3,11 @@
 @section('content')
     <div class="container">
         <h2>最新の投稿</h2>
-        @if (have_posts())
-            @while (have_posts())
-                {{ the_post() }}
-                <article>
-                    <a href="{{ the_permalink() }}">
-                        @php
-                            // Get talent info.
-                            $talent_array = get_the_category();
-                            foreach ($talent_array as $talent) {
-                                $talent_id = $talent->cat_ID;
-                                $talent_name = $talent->name;
-                                $profile_img = get_category_img( $talent_id );
-                            }
-
-                            // Desicion for profile image exists.
-                            if ( $profile_img ) {
-                            } else {
-                                $placeholder_img_data = get_page_by_path('placeholder_img_profile', 'OBJECT', 'post');
-                                $placeholder_id = $placeholder_img_data->ID;
-                                $profile_img = wp_get_attachment_image_src( $placeholder_id, 'full')[0];
-                            }
-
-                            // Get rating of current post
-                            $rating_latest = get_field('rating_latest');
-                        @endphp
-
-                        <h3>{{ the_title() }}</h3>
-                        <div>
-                            <p><img src="{{ $profile_img }}" alt=""></p>
-                            <p>{{ $talent_name }}</p>
-                        </div>
-                        <div>{{ $rating_latest }}</div>
-                        <div>{{ the_excerpt() }}</div>
-                    </a>
-                </article>
-            @endwhile
-        @endif
+            @if (have_posts())
+                @while (have_posts())
+                    {{ the_post() }}
+                    @include('article_card')
+                @endwhile
+            @endif
     </div>
 @endsection
