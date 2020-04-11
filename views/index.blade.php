dc6b87bb34a6a4a23c885c0649d10c72fc086a12
KoheyAoyama <kou_713@hotmail.com>
2020-04-11 13:45:18 +0900

    Modify article card style.

--- views/index.blade.php
+++ views/index.blade.php
@@ -3,11 +3,13 @@
 @section('content')
     <div class="p-feed">
         <h2 class="p-feed__title">最新の投稿</h2>
-        @if (have_posts())
-            @while (have_posts())
-                {{ the_post() }}
-                @include('article_card')
-            @endwhile
-        @endif
+        <div class="p-feed__list">
+            @if (have_posts())
+                @while (have_posts())
+                    {{ the_post() }}
+                    @include('article_card')
+                @endwhile
+            @endif
+        </div>
     </div>
 @endsection
