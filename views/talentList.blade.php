214ac31e53d333854ab0e89ce4f59c889a433912
KoheyAoyama <kou_713@hotmail.com>
2020-04-11 21:20:42 +0900

    Fix bug

--- views/talentList.blade.php
+++ views/talentList.blade.php
@@ -21,55 +21,56 @@
 @section('content')
     <div class="talentList">
         <h1 class="talentList__title">{{ the_title() }}</h1>
+        <section class="talentList__content">
+            @foreach ($talent_list as $talent)
+                @php
+                    $talent_id = $talent->cat_ID;
+                    $talent_name = $talent->name;
+                    $profile_img = get_category_img( $talent_id );
+                    $office = get_field('talent_office', 'category_'. $talent_id);
 
-        @foreach ($talent_list as $talent)
-            @php
-                $talent_id = $talent->cat_ID;
-                $talent_name = $talent->name;
-                $profile_img = get_category_img( $talent_id );
-                $office = get_field('talent_office', 'category_'. $talent_id);
+                    // Get rating_latest value by getting latest post of the talent.
+                    $args_get_latest_post = array(
+                        'posts_per_page'   => 1,
+                        'offset'           => 0,
+                        'category'         => $talent_id,
+                        'category_name'    => '',
+                        'orderby'          => 'date',
+                        'order'            => 'DESC',
+                        'include'          => '',
+                        'exclude'          => '',
+                        'meta_key'         => '',
+                        'meta_value'       => '',
+                        'post_type'        => 'post',
+                        'post_mime_type'   => '',
+                        'post_parent'      => '',
+                        'author'	       => '',
+                        'post_status'      => 'publish',
+                        'suppress_filters' => true 
+                    );
+                    $latest_post = get_posts( $args_get_latest_post );
+                    $latest_post_id = $latest_post[0]->ID;
+                    if ( get_field('rating_latest', $latest_post_id) ) {
+                        $rating_latest = get_field('rating_latest', $latest_post_id);
+                    } else {
+                        $rating_latest = '未評価';
+                    }
 
-                // Get rating_latest value by getting latest post of the talent.
-                $args_get_latest_post = array(
-                    'posts_per_page'   => 1,
-                    'offset'           => 0,
-                    'category'         => $talent_id,
-                    'category_name'    => '',
-                    'orderby'          => 'date',
-                    'order'            => 'DESC',
-                    'include'          => '',
-                    'exclude'          => '',
-                    'meta_key'         => '',
-                    'meta_value'       => '',
-                    'post_type'        => 'post',
-                    'post_mime_type'   => '',
-                    'post_parent'      => '',
-                    'author'	       => '',
-                    'post_status'      => 'publish',
-                    'suppress_filters' => true 
-                );
-                $latest_post = get_posts( $args_get_latest_post );
-                $latest_post_id = $latest_post[0]->ID;
-                if ( get_field('rating_latest', $latest_post_id) ) {
-                    $rating_latest = get_field('rating_latest', $latest_post_id);
-                } else {
-                    $rating_latest = '未評価';
-                }
-
-                // Desicion for profile image exists.
-                if ( $profile_img ) {
-                } else {
-                    $placeholder_img_data = get_page_by_path('placeholder_img_profile', 'OBJECT', 'post');
-                    $placeholder_id = $placeholder_img_data->ID;
-                    $profile_img = wp_get_attachment_image_src( $placeholder_id, 'full')[0];
-                }
-            @endphp
-            
-            <div class="talentList__content">
-                <a href="{{ get_category_link( $talent_id ) }}">
-                    @include('module.profileMedium')
-                </a>
-            </div>
-        @endforeach
+                    // Desicion for profile image exists.
+                    if ( $profile_img ) {
+                    } else {
+                        $placeholder_img_data = get_page_by_path('placeholder_img_profile', 'OBJECT', 'post');
+                        $placeholder_id = $placeholder_img_data->ID;
+                        $profile_img = wp_get_attachment_image_src( $placeholder_id, 'full')[0];
+                    }
+                @endphp
+                
+                <div class="talentProfile">
+                    <a href="{{ get_category_link( $talent_id ) }}">
+                        @include('module.profileMedium')
+                    </a>
+                </div>
+            @endforeach
+        </section>
     </div>
 @endsection
