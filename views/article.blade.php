49244367dc1c5da66a94048aab6f9f97e15521e1
KoheyAoyama <kou_713@hotmail.com>
2020-04-11 16:29:32 +0900

    Refactore article.blade.php

--- views/article.blade.php
+++ views/article.blade.php
@@ -7,13 +7,13 @@
 @endphp
 
 <article class="p-article">
-    <a class="p-article__inner-content" href="{{ the_permalink() }}">
-        <div class="p-article-content">
-            <h3 class="p-article-content__title">{{ the_title() }}</h3>
-            <div class="p-article-content__body">{{ the_excerpt() }}</div>
+    <a class="p-article__innerContent" href="{{ the_permalink() }}">
+        <div class="p-articleExcerpt">
+            <h3 class="p-articleExcerpt__title">{{ the_title() }}</h3>
+            <div class="p-articleExcerpt__body">{{ the_excerpt() }}</div>
         </div>
     </a>
-    <a class="p-article__inner-profile" href="{{ get_category_link( $talent_id ) }}">
-        @include('talent-profile--small')
+    <a class="p-article__innerProfile" href="{{ get_category_link( $talent_id ) }}">
+        @include('profileSmall')
     </a>
 </article>
--- views/profileSmall.blade.php
+++ views/profileSmall.blade.php
@@ -20,15 +20,15 @@
     $rating_setting = 'width: ' . ($rating_latest * 2) . '0%';
 @endphp
 
-<div class="p-article-profile">
+<div class="p-profileSmall">
     <div>
-        <p><img class="p-article-profile__image" src="{{ $profile_img }}" alt=""></p>
+        <p><img class="p-profileSmall__image" src="{{ $profile_img }}" alt=""></p>
     </div>
     <div>
-        <p class="p-article-profile__name">{{ $talent_name }}</p>
-        <div class="star-rating">
-            <div class="star-rating-front" style="{{ $rating_setting }}">★★★★★</div>
-            <div class="star-rating-back">★★★★★</div>
+        <p class="p-profileSmall__name">{{ $talent_name }}</p>
+        <div class="p-profileSmall__starRating">
+            <div class="p-profileSmall__starRating--front" style="{{ $rating_setting }}">★★★★★</div>
+            <div class="p-profileSmall__starRating--back">★★★★★</div>
         </div>
     </div>
 </div>
