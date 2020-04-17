8cf0a98ed1a7a477ff8f287d7aea888570f6cbf7
KoheyAoyama <kou_713@hotmail.com>
2020-04-17 19:46:43 +0900

    #5 Refactor articleItem files. Move readMore css style to indipendent file.

--- views/module/articleItem.blade.php
+++ views/module/articleItem.blade.php
@@ -1,11 +1,11 @@
 <article class="p-article">
-    <a class="p-article__innerContent" href="{{ the_permalink() }}">
-        <div class="p-articleExcerpt">
-            <h3 class="p-articleExcerpt__title">{{ the_title() }}</h3>
-            <div class="p-articleExcerpt__body">{{ the_excerpt() }}</div>
+    <a href="{{ the_permalink() }}">
+        <div class="p-article__excerpt">
+            <h3 class="p-article__excerpt__title">{{ the_title() }}</h3>
+            <div class="p-article__excerpt__body">{{ the_excerpt() }}</div>
         </div>
     </a>
-    <a class="p-article__innerProfile" href="{{ get_category_link( $talent_id ) }}">
+    <a href="{{ get_category_link( $talent_id ) }}">
         @include('module.profileSmall')
     </a>
 </article>
