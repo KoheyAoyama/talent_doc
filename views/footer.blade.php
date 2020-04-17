d247b81ded581c030647da8494bf152e9e9ee9ef
KoheyAoyama <kou_713@hotmail.com>
2020-04-17 13:36:35 +0900

    #5 Refactor footer files class name.

--- views/footer.blade.php
+++ views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="l-footer">
   <div class="p-footer">
-    <div class="p-footer-rights">
-      <p class="p-footer-rights__text">All rights reserved 2020</p>
+    <div class="p-copyrights">
+      <p class="p-copyrights__text">All rights reserved 2020</p>
     </div>
   </div>
 </footer>
