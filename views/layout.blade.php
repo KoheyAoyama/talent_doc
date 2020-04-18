aa7825114faa670640e8609cddf2ac64ca4c3652
KoheyAoyama <kou_713@hotmail.com>
2020-04-18 23:00:27 +0900

    Add Twitter meta tags

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -1,9 +1,19 @@
+@php
+  $ogp_img_data = get_page_by_path('ogp_img', 'OBJECT', 'post');
+  $ogp_img_id = $ogp_img_data->ID;
+  $ogp_img = wp_get_attachment_image_src( $ogp_img_id, 'full')[0];    
+@endphp
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="twitter:card" content="summary_large_image"></meta>
+    <meta name="twitter:title" content="{{ the_title() }}">
+    <meta name="twitter:description" content="お笑い芸人について知るならTalendDoc（タレントドック）">
+    <meta name="twitter:image" content="{{ $ogp_img }}">
     <title>{{ bloginfo('name') }}</title>
     <link rel="stylesheet" href="{{ get_template_directory_uri() }}/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&display=swap" rel="stylesheet">
