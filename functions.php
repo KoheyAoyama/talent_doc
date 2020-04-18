7999da2904845d1854db8bb7ede9b8355743251b
KoheyAoyama <kou_713@hotmail.com>
2020-04-18 17:10:11 +0900

    #15 Spage view on admin panel

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 
+
+/*----------------------------------------
+Enable Blade template engine
+----------------------------------------*/
 require_once(__DIR__ . '/vendor/autoload.php');
-
 use Jenssegers\Blade\Blade;
-
-/**
- * Bladeテンプレートをレンダリングする
- */
+// Render Blade template
 if (!function_exists('render_blade')) {
     function render_blade($template_name)
     {
@@ -16,6 +16,10 @@ if (!function_exists('render_blade')) {
     }
 }
 
+/*----------------------------------------
+Theme functions
+----------------------------------------*/
+// Article excerpt setting
 function new_excerpt_more($more) {
 	return '</br><span class="read-more">続きを読む</span>';
 }
@@ -59,3 +63,27 @@ function is_bot() {
     }
     return false;
   }
+
+/*----------------------------------------
+Custom admin panel settings
+----------------------------------------*/
+// Edit post admin panel column
+function manage_posts_columns($columns) {
+  $columns['post_views_count'] = 'Page View';
+  return $columns;
+  }
+  function add_column($column_name, $post_id) {
+    /*View数呼び出し*/
+    if ( $column_name == 'post_views_count' ) {
+        $stitle = get_field("page_view", $post_id);
+    }
+    if ( isset($stitle) && $stitle ) {
+      echo attribute_escape($stitle);
+    }
+    else {
+      echo __('None');
+    }
+  }
+  add_filter( 'manage_posts_columns', 'manage_posts_columns' );
+  add_action( 'manage_posts_custom_column', 'add_column', 10, 2 );
+    
