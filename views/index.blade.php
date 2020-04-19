7ebdc4725c156ad77f94a06fb71e78c1c5c36327
KoheyAoyama <kou_713@hotmail.com>
2020-04-19 16:19:06 +0900

    Test git ftp

--- views/index.blade.php
+++ views/index.blade.php
@@ -1,6 +1,5 @@
 @extends('layout')
 
 @section('content')
-    <img src="{{ get_template_directory_uri() }}/src/img/page_hero.jpg" alt="unko">
     @include('feed',['feedTitle'=>'最新の投稿'])
 @endsection
